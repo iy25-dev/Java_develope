dc5093f531e5a0e9c69afebe78080f273bdc9b82
DELL <DELL@WachiPy-DELL>
2025-12-08 15:31:21 +0900

    3.Java導入 自己紹介文

---
+++ HelloWorld_YutaIsomatsu/src/sample/SelfIntroduction.java
@@ -0,0 +1,22 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// 氏名
+		String name = "礒松 優太";
+		// 年齢
+		int age = 27;
+		// 趣味
+		String hobby = "野球観戦";
+
+		/*
+		* 自己紹介を生成する処理
+		*/
+		
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+	}
+
+}
